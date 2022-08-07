f1a3a3c0cd0d329ab12c09264d08ce2755ad16f8
Azalurg <patryk31415@gmial.com>
2022-08-07 09:32:04 +0000

    Basic db configuration Add docker-compose and init.sql

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # DB
+
+## Example database.env file:
+
+``
+POSTGRES_USER=unicorn_user
+POSTGRES_PASSWORD=magical_password
+POSTGRES_DB=rainbow_database
+``
---
+++ db/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS 'users' (
+	'id' int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	'username' varchar(64) NOT NULL
+)
+
+INSERT INTO 'users' ('id' 'username') VALUES
+(1, 'admin');
