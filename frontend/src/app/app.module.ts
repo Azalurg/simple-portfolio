4e418dbb616c4922394b23b2afedeac4bf50ed76
Azalurg <patryk31415@gmial.com>
2022-08-16 21:25:53 +0000

    Create basic componets (home, navbar, footer)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
